1f21a026ca9d7ef71c105cbb311ef8d5ec3a380a
孙鹏程 <13027699936@163.com>
2019-05-28 15:54:01 +0800

    component

--- app/src/main/java/com/component/MainActivity.java
+++ app/src/main/java/com/component/MainActivity.java
@@ -1,13 +1,82 @@
 package com.component;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.baselibrary.mvp.BaseActivity;
+import com.baselibrary.mvp.BasePresenter;
+import com.baselibrary.service.ApiManager;
+import com.baselibrary.service.MObserver;
+import com.baselibrary.skin.SkinManager;
+import com.baselibrary.utils.NotificationDialogUtils;
+import com.component.bean.BannerBean;
+import com.component.net.RetrofitManage;
+import com.google.gson.Gson;
+
+import java.io.File;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.change_theme)
+    Button changeTheme;
+    @BindView(R.id.change_recover)
+    Button changeRecover;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    public void onCreate() {
+
+
+    }
+    @Override
+    public BasePresenter initPresenter() {
+        return null;
+    }
+
+    @OnClick({R.id.change_theme, R.id.change_recover,R.id.change_theme_reset})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.change_theme:
+                SkinManager.getInstance()
+                        .loadSkin(Environment.getExternalStorageDirectory()
+                                .getAbsolutePath()+ File.separator+"blue-skin.skin");
+                break;
+            case R.id.change_theme_reset:
+                SkinManager.getInstance().restoreDefaultTheme();
+                break;
+            case R.id.change_recover:
+               getBanner();
+                break;
+        }
+    }
+
+    public void getBanner() {
+        RetrofitManage.getInstance()
+                .bannerData()
+                .compose(ApiManager.<BannerBean>toMainThread())
+                .subscribe(new MObserver<BannerBean>(null) {
+                    @Override
+                    protected void success(BannerBean bannerBean) {
+                        Log.d("SSSSSSSS", "success: "+new Gson().toJson(bannerBean));
+                    }
+
+                    @Override
+                    protected void error(String error) {
+                        Log.d("SSSSSS", "error: "+error);
+                    }
+                });
+
+    }
+
+
 }
